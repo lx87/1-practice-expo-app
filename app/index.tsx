f9891a43a757c614c7932e107ea5e70ff0def0f7
lx87 <limarx87@gmail.com>
2025-11-15 09:38:42 +0300

    final

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,6 @@
-import CardText from "@/components/CardText";
-import MainImage from "@/components/MainImage";
-import Skills from "@/components/Skills";
-
 import { styles } from "@/static/styles";
-
 import { StatusBar } from 'expo-status-bar';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
@@ -13,9 +8,25 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.card}>
-        <MainImage />
-        <CardText />
-        <Skills />
+        <Image
+          style={styles.avatar}
+          source={require('@/assets/images/ava.jpg')}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.name}>Рами Лайли</Text>
+          <Text style={styles.title}>Senior Fullstack Developer</Text>
+          <Text style={styles.bio}>
+            Люблю Жену, React и TypeScript. Разрабатываю приложения уже 2 года.
+          </Text>
+        </View>
+        <View style={styles.skillsContainer}>
+          <Text style={styles.skill}>TypeScript + JavaScript</Text>
+          <Text style={styles.skill}>React</Text>
+          <Text style={styles.skill}>Rust</Text>
+          <Text style={styles.skill}>Tauri 2.0</Text>
+          <Text style={styles.skill}>Electron JS</Text>
+          <Text style={styles.skill}>UI/UX</Text>
+        </View>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Подписаться</Text>
         </TouchableOpacity>
